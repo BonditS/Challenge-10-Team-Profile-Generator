3bba835a2e8cd0d0657c83100947584c1a55d13c
AlayC <bsinlee@icloud.com>
2022-11-10 00:28:49 -0600

    Employee.method()

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -14,5 +14,34 @@ describe('Employee', () => {
     } )
 })
 describe('getName', () =>{
-    it(`getName() method should return employee name`)
+    it(`should return employee name`, ()=>{
+        //arrange
+        const employeeName = new Employee(name, id, email)
+
+        //assert
+        expect(employeeName.getName()).toEqual(name)
+    })
+})
+describe('getId', () =>{
+    it(`should return employee id`, ()=>{
+        //arrange
+        const employeeId = new Employee(name, id, email)
+        //assert
+        expect(employeeId.getId()).toEqual(id)
+    })
+})
+describe('getEmail',()=>{
+    it(`should return employee email`, ()=>{
+        //arrange
+        const employeeEmail = new Employee(name, id, email)
+        //assert
+        expect(employeeEmail.getEmail()).toEqual(email)
+    })
+})
+describe('getRole',()=>{
+    it(`should return employee role`, ()=>{
+        //arrange
+        const employeeRole = new Employee(name, id, email)
+        expect(employeeRole.getRole()).toEqual('Employee')
+    })
 })
